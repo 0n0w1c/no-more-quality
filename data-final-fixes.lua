8e8f1887ae2592fef5e6accd515d9f4d61620de1
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-12-17 12:09:39 -0600

    Version 0.0.1

---
+++ data-final-fixes.lua
@@ -0,0 +1,50 @@
+if not mods["quality"] then return end
+
+for k, v in pairs(data.raw.quality) do
+    if k == "normal" or k == "quality-unknown" then
+        v.hidden = true
+        v.next = nil
+    else
+        data.raw.quality[k] = nil
+    end
+end
+
+for _, v in pairs(data.raw.module) do
+    if v.effect then
+        v.effect.quality = nil
+    end
+end
+
+data.raw.module["quality-module"] = nil
+data.raw.module["quality-module-2"] = nil
+data.raw.module["quality-module-3"] = nil
+
+data.raw.recipe["quality-module"] = nil
+data.raw.recipe["quality-module-2"] = nil
+data.raw.recipe["quality-module-3"] = nil
+data.raw.recipe["quality-module-recycling"] = nil
+data.raw.recipe["quality-module-2-recycling"] = nil
+data.raw.recipe["quality-module-3-recycling"] = nil
+
+data.raw.technology["quality-module"] = nil
+data.raw.technology["quality-module-2"] = nil
+data.raw.technology["quality-module-3"] = nil
+data.raw.technology["epic-quality"] = nil
+data.raw.technology["legendary-quality"] = nil
+
+data.raw["tips-and-tricks-item"]["quality"] = nil
+data.raw["tips-and-tricks-item"]["quality-modules"] = nil
+data.raw["tips-and-tricks-item"]["quality-factoriopedia"] = nil
+data.raw["tips-and-tricks-item"]["quality-probabilities"] = nil
+
+data.raw["produce-achievement"]["crafting-with-quality"] = nil
+data.raw["produce-achievement"]["my-modules-are-legendary"] = nil
+
+if mods["space-age"] then
+    data.raw["equip-armor-achievement"]["look-at-my-shiny-rare-armor"] = nil
+    data.raw["module-transfer-achievement"]["make-it-better"] = nil
+    data.raw["place-equipment-achievement"]["no-room-for-more"] = nil
+    data.raw["use-item-achievement"]["todays-fish-is-trout-a-la-creme"] = nil
+end
+
+data.raw.sprite["quality_info"].filename = "__no-more-quality__/graphics/icons/faux-quality-info-blue.png"
---
+++ readme.md
@@ -0,0 +1,16 @@
+# No More Quality - A fork of No Quality v0.1.3 by grandseiken
+
+Enjoy Space Age without Quality!
+
+## Removes:
+
+* Quality modules
+* Quality tiers other than "normal"
+* Related technologies and achievements
+* Quality tooltips
+* Factoriopedia quality blue diamond
+
+## Preserves:
+
+* Recycler
+* Recycling recipes
