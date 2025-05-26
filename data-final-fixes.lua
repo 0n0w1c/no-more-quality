b816a410b944a7e50a181d2e870a81dedc2bf31d
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2025-05-25 23:36:28 -0500

    Version 0.0.5

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,4 +1,5 @@
 if not mods["quality"] then return end
+if mods["crushing-industry"] and settings.startup["crushing-industry-space-crusher"].value == true then return end
 
 for key, value in pairs(data.raw.quality) do
     if key == "normal" or key == "quality-unknown" then
